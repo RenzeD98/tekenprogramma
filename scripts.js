0303cd20d51811b7cae0c43c965febeb0bdea1f3
RenzeD98 <renze.d98@gmail.com>
2019-06-10 15:01:44 +0200

    added line and start of cirkel

--- scripts.js
+++ scripts.js
@@ -3,17 +3,58 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
 var rect = /** @class */ (function () {
-    function rect(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+    function rect() {
     }
-    rect.prototype.createRect = function () {
-        c.fillRect(this.x, this.y, this.width, this.height);
+    // constructor(x:number, y:number, width:number, height:number){
+    //     this.x = x;
+    //     this.y = y;
+    //     this.width = width;
+    //     this.height = height;
+    // }
+    //
+    // createRect(){
+    //     c.fillRect(this.x, this.y, this.width, this.height);
+    // }
+    rect.prototype.createRect = function (x, y, width, height, color) {
+        c.fillStyle = color;
+        c.fillRect(x, y, width, height);
     };
     return rect;
 }());
-var rectangle = new rect(50, 80, 200, 400);
-rectangle.createRect();
+var line = /** @class */ (function () {
+    function line(x, y, color) {
+        this.xStart = x;
+        this.yStart = y;
+        this.color = color;
+        c.beginPath();
+        c.moveTo(this.xStart, this.yStart);
+        c.strokeStyle = color;
+    }
+    line.prototype.createAnchor = function (x, y) {
+        c.lineTo(x, y);
+        c.stroke();
+    };
+    return line;
+}());
+var Arc = /** @class */ (function () {
+    function Arc() {
+    }
+    Arc.prototype.createStrokeArc = function () {
+    };
+    Arc.prototype.createFilledArc = function (x, y, radius, startAngle, endAngle, counterClockWise) {
+        c.beginPath();
+        c.arc(x, y, radius, startAngle, endAngle, counterClockWise);
+    };
+    return Arc;
+}());
+// Create rectangle with variables in constructor
+// let rectangle = new rect(50, 80, 200, 400);
+//     rectangle.createRect();
+// Create rectangle with variables in function
+var rectangle = new rect();
+rectangle.createRect(50, 80, 200, 400, 'green');
+// Create Line with different anchorpoints
+var newLine = new line(40, 50, 'blue');
+newLine.createAnchor(100, 150);
+newLine.createAnchor(130, 80);
 //# sourceMappingURL=scripts.js.map
--- scripts.ts
+++ scripts.ts
@@ -11,18 +11,72 @@ class rect
     y:number;
     width:number;
     height:number;
+    color:string;
 
-    constructor(x: number, y: number, width: number, height: number){
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
+    // constructor(x:number, y:number, width:number, height:number){
+    //     this.x = x;
+    //     this.y = y;
+    //     this.width = width;
+    //     this.height = height;
+    // }
+    //
+    // createRect(){
+    //     c.fillRect(this.x, this.y, this.width, this.height);
+    // }
 
-    createRect(){
-        c.fillRect(this.x, this.y, this.width, this.height);
+    createRect(x:number, y:number, width:number, height:number, color:string){
+        c.fillStyle = color;
+        c.fillRect(x, y, width, height);
     }
 }
 
-let rectangle = new rect(50, 80, 200, 400);
-    rectangle.createRect();
+class line
+{
+    xStart: number;
+    yStart: number;
+    color: string;
+
+    constructor(x:number, y:number, color:string) {
+        this.xStart = x;
+        this.yStart = y;
+        this.color = color;
+
+        c.beginPath();
+        c.moveTo(this.xStart, this.yStart);
+        c.strokeStyle = color;
+    }
+
+
+    createAnchor(x:number, y:number){
+        c.lineTo(x, y);
+        c.stroke();
+    }
+}
+
+class Arc
+{
+    createStrokeArc(){
+
+    }
+
+    createFilledArc(x:number, y:number, radius:number, startAngle:number, endAngle:number, counterClockWise: boolean){
+        c.beginPath();
+
+        c.arc(x,y,radius,startAngle,endAngle,counterClockWise);
+    }
+}
+
+// Create rectangle with variables in constructor
+// let rectangle = new rect(50, 80, 200, 400);
+//     rectangle.createRect();
+
+
+// Create rectangle with variables in function
+let rectangle = new rect();
+    rectangle.createRect(50, 80, 200, 400, 'green');
+
+// Create Line with different anchorpoints
+let newLine = new line(40, 50, 'blue');
+    newLine.createAnchor(100, 150);
+    newLine.createAnchor(130, 80);
+
