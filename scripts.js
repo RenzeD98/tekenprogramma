5fdc13fa811ea9ac19b96ee4ea98040b94406ddd
RenzeD98 <renze.d98@gmail.com>
2019-06-11 19:10:57 +0200

    added free drawing function

--- scripts.js
+++ scripts.js
@@ -1,28 +1,47 @@
+//initializing size and the context of the canvas
 var canvas = document.getElementById('canvas');
+var c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-var c = canvas.getContext('2d');
-var rect = /** @class */ (function () {
-    function rect() {
+//tool type listeren
+var usedTool = 0;
+function changeTool(radio) {
+    usedTool = radio.value;
+}
+// Mouse event listener
+var mouse = {
+    x: undefined,
+    y: undefined
+};
+window.addEventListener('mousemove', function (event) {
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+var DrawObject = /** @class */ (function () {
+    function DrawObject(x, y) {
+        this.x = x;
+        this.y = y;
     }
-    // constructor(x:number, y:number, width:number, height:number){
-    //     this.x = x;
-    //     this.y = y;
-    //     this.width = width;
-    //     this.height = height;
-    // }
-    //
-    // createRect(){
-    //     c.fillRect(this.x, this.y, this.width, this.height);
-    // }
-    rect.prototype.createRect = function (x, y, width, height, color) {
-        c.fillStyle = color;
-        c.fillRect(x, y, width, height);
-    };
-    return rect;
+    return DrawObject;
 }());
-var line = /** @class */ (function () {
-    function line(x, y, color) {
+var Rect = /** @class */ (function () {
+    function Rect(x, y, width, height, color) {
+        this.lineWidth = 5;
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    Rect.prototype.createRect = function () {
+        c.fillStyle = this.color;
+        c.lineWidth = this.lineWidth;
+        c.fillRect(this.x, this.y, this.width, this.height);
+    };
+    return Rect;
+}());
+var Line = /** @class */ (function () {
+    function Line(x, y, color) {
         this.xStart = x;
         this.yStart = y;
         this.color = color;
@@ -30,31 +49,76 @@ var line = /** @class */ (function () {
         c.moveTo(this.xStart, this.yStart);
         c.strokeStyle = color;
     }
-    line.prototype.createAnchor = function (x, y) {
+    Line.prototype.createAnchor = function (x, y) {
         c.lineTo(x, y);
         c.stroke();
     };
-    return line;
+    return Line;
 }());
 var Arc = /** @class */ (function () {
-    function Arc() {
+    function Arc(x, y, radius, startAngle, endAngle, counterClockWise, fillColor, outlineColor) {
+        this.lineWidth = 5;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.startAngle = startAngle;
+        this.endAngle = endAngle;
+        this.counterClockWise = counterClockWise;
+        this.fillColor = fillColor;
+        this.outlinecolor = outlineColor;
     }
-    Arc.prototype.createStrokeArc = function () {
-    };
-    Arc.prototype.createFilledArc = function (x, y, radius, startAngle, endAngle, counterClockWise) {
+    Arc.prototype.createArc = function (outline, fill) {
         c.beginPath();
-        c.arc(x, y, radius, startAngle, endAngle, counterClockWise);
+        c.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle, this.counterClockWise);
+        c.lineWidth = this.lineWidth;
+        if (outline) {
+            c.strokeStyle = this.outlinecolor;
+            c.stroke();
+        }
+        if (fill) {
+            c.fillStyle = this.fillColor;
+            c.fill();
+        }
     };
     return Arc;
 }());
-// Create rectangle with variables in constructor
-// let rectangle = new rect(50, 80, 200, 400);
+// /**
+//  * Create rectangle with variables in constructor
+//  */
+// let rectangle = new Rect(500, 500, 200, 400, 'green');
 //     rectangle.createRect();
-// Create rectangle with variables in function
-var rectangle = new rect();
-rectangle.createRect(50, 80, 200, 400, 'green');
-// Create Line with different anchorpoints
-var newLine = new line(40, 50, 'blue');
-newLine.createAnchor(100, 150);
-newLine.createAnchor(130, 80);
+//
+// /**
+//  * Create Line with different anchorpoints
+//  */
+// let newLine = new Line(40, 50, 'blue');
+//     newLine.createAnchor(100, 150);
+//     newLine.createAnchor(130, 80);
+//
+// /**
+//  * Create arc
+//  */
+// let newArc = new Arc(400, 400, 40, 0, Math.PI * 2, false, 'yellow', 'green');
+//     newArc.createArc(true, true);
+/**
+ * Free draw
+ */
+var beginLine = true;
+var line;
+window.addEventListener('mousemove', function (event) {
+    if (usedTool == 1) {
+        if (event.buttons === 1) {
+            if (beginLine) {
+                line = new Line(mouse.x, mouse.y, 'black');
+                beginLine = false;
+            }
+            else {
+                line.createAnchor(mouse.x, mouse.y);
+            }
+        }
+        else {
+            beginLine = true;
+        }
+    }
+});
 //# sourceMappingURL=scripts.js.map
--- scripts.ts
+++ scripts.ts
@@ -1,40 +1,66 @@
-let canvas : HTMLCanvasElement = <HTMLCanvasElement> document.getElementById('canvas');
+//initializing size and the context of the canvas
+let canvas:HTMLCanvasElement = <HTMLCanvasElement> document.getElementById('canvas');
+let c = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let c = canvas.getContext('2d');
+//tool type listeren
+let usedTool:number = 0;
+function changeTool(radio){
+    usedTool = radio.value;
+}
 
+// Mouse event listener
+let mouse = {
+    x: undefined,
+    y: undefined
+};
 
-class rect
+window.addEventListener('mousemove', function(event){
+    mouse.x = event.x;
+    mouse.y = event.y;
+});
+
+class DrawObject
+{
+    x:number;
+    y:number;
+
+    constructor(x:number, y:number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Rect
 {
     x:number;
     y:number;
     width:number;
     height:number;
     color:string;
+    lineWidth:number = 5;
 
-    // constructor(x:number, y:number, width:number, height:number){
-    //     this.x = x;
-    //     this.y = y;
-    //     this.width = width;
-    //     this.height = height;
-    // }
-    //
-    // createRect(){
-    //     c.fillRect(this.x, this.y, this.width, this.height);
-    // }
+    constructor(x:number, y:number, width:number, height:number, color:string){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
 
-    createRect(x:number, y:number, width:number, height:number, color:string){
-        c.fillStyle = color;
-        c.fillRect(x, y, width, height);
+    createRect(){
+        c.fillStyle = this.color;
+        c.lineWidth = this.lineWidth;
+        c.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 
-class line
+class Line
 {
-    xStart: number;
-    yStart: number;
-    color: string;
+    xStart:number;
+    yStart:number;
+    color:string;
 
     constructor(x:number, y:number, color:string) {
         this.xStart = x;
@@ -55,28 +81,83 @@ class line
 
 class Arc
 {
-    createStrokeArc(){
+    x:number;
+    y:number;
+    radius:number;
+    startAngle:number;
+    endAngle:number;
+    counterClockWise:boolean;
+    fillColor:string;
+    outlinecolor:string;
+    lineWidth:number = 5;
 
+    constructor(x:number, y:number, radius:number, startAngle:number, endAngle:number, counterClockWise:boolean, fillColor:string, outlineColor:string){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.startAngle = startAngle;
+        this.endAngle = endAngle;
+        this.counterClockWise = counterClockWise;
+        this.fillColor = fillColor;
+        this.outlinecolor = outlineColor;
     }
 
-    createFilledArc(x:number, y:number, radius:number, startAngle:number, endAngle:number, counterClockWise: boolean){
+    createArc(outline:boolean, fill:boolean){
         c.beginPath();
+        c.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle, this.counterClockWise);
+        c.lineWidth = this.lineWidth;
 
-        c.arc(x,y,radius,startAngle,endAngle,counterClockWise);
+        if(outline){
+            c.strokeStyle = this.outlinecolor;
+            c.stroke();
+        }
+        if(fill){
+            c.fillStyle = this.fillColor;
+            c.fill();
+        }
     }
 }
 
-// Create rectangle with variables in constructor
-// let rectangle = new rect(50, 80, 200, 400);
+
+// /**
+//  * Create rectangle with variables in constructor
+//  */
+// let rectangle = new Rect(500, 500, 200, 400, 'green');
 //     rectangle.createRect();
+//
+// /**
+//  * Create Line with different anchorpoints
+//  */
+// let newLine = new Line(40, 50, 'blue');
+//     newLine.createAnchor(100, 150);
+//     newLine.createAnchor(130, 80);
+//
+// /**
+//  * Create arc
+//  */
+// let newArc = new Arc(400, 400, 40, 0, Math.PI * 2, false, 'yellow', 'green');
+//     newArc.createArc(true, true);
 
 
-// Create rectangle with variables in function
-let rectangle = new rect();
-    rectangle.createRect(50, 80, 200, 400, 'green');
+/**
+ * Free draw
+ */
+let beginLine:boolean = true;
+let line:Line;
+window.addEventListener('mousemove', function (event) {
+    if (usedTool == 1) {
+        if (event.buttons === 1) {
+            if (beginLine) {
+                line = new Line(mouse.x, mouse.y, 'black');
+                beginLine = false;
+            } else {
+                line.createAnchor(mouse.x, mouse.y);
+            }
+        } else {
+            beginLine = true;
+        }
+    }
+});
 
-// Create Line with different anchorpoints
-let newLine = new line(40, 50, 'blue');
-    newLine.createAnchor(100, 150);
-    newLine.createAnchor(130, 80);
+
 
